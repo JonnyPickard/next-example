5357508120906f1fd4d7a5f7d8dc2c0cd9c1cc44
Jonny Pickard <jonnypickard@notonthehighstreet.com>
2020-01-15 10:53:26 +0000

    Initial commit

---
+++ .next/static/development/pages/hello.js
@@ -0,0 +1,90 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/hello.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhello&absolutePagePath=%2FUsers%2Fjonnyp%2Fprojects%2Fnext-portfolio%2Fpages%2Fhello.js!./":
+/*!**********************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhello&absolutePagePath=%2FUsers%2Fjonnyp%2Fprojects%2Fnext-portfolio%2Fpages%2Fhello.js ***!
+  \**********************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/hello", function() {
+      var mod = __webpack_require__(/*! ./pages/hello.js */ "./pages/hello.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/hello.js */ "./pages/hello.js", function() {
+          if(!next.router.components["/hello"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/hello.js */ "./pages/hello.js")
+          next.router.update("/hello", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_ef0ff7c60362f24a921f ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_ef0ff7c60362f24a921f */ "dll-reference dll_ef0ff7c60362f24a921f"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/hello.js":
+/*!************************!*\
+  !*** ./pages/hello.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/jonnyp/projects/next-portfolio/pages/hello.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Hello = function Hello() {
+  return __jsx("h2", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    },
+    __self: this
+  }, "Hello, World!");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Hello);
+
+/***/ }),
+
+/***/ 0:
+/*!**************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fhello&absolutePagePath=%2FUsers%2Fjonnyp%2Fprojects%2Fnext-portfolio%2Fpages%2Fhello.js ***!
+  \**************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fhello&absolutePagePath=%2FUsers%2Fjonnyp%2Fprojects%2Fnext-portfolio%2Fpages%2Fhello.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhello&absolutePagePath=%2FUsers%2Fjonnyp%2Fprojects%2Fnext-portfolio%2Fpages%2Fhello.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_ef0ff7c60362f24a921f":
+/*!*******************************************!*\
+  !*** external "dll_ef0ff7c60362f24a921f" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_ef0ff7c60362f24a921f;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=hello.js.map
